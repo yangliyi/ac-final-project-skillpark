cba4f0ef338f230f73d9b89b668da2070a4d2ba3
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-10-15 08:44:25 +0800

    Layout adjustment

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,7 +3,6 @@ class Picture < ActiveRecord::Base
   belongs_to :skill
 
   has_attached_file :image,
-    styles: { medium: "300x300>", thumb: "100x100>" },
     :path => ":rails_root/public/images/:id/:filename",
     :url  => "/images/:id/:filename"
 
