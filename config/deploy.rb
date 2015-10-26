e07094e589af63718bbe2175cffa84fa5529f709
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-10-26 16:20:56 +0800

    Include public/images for skills' pictures

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :deploy_to, '/home/deploy/skillpark'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/fb.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/images')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
