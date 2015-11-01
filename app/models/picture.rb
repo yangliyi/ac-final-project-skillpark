2ee3a36c712ecbe21dd23cf7ccfa214520aa366c
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-11-02 00:17:17 +0800

    Limit pictures size and add username to skill show page

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,7 +4,8 @@ class Picture < ActiveRecord::Base
 
   has_attached_file :image,
     :path => ":rails_root/public/images/:id/:filename",
-    :url  => "/images/:id/:filename"
+    :url  => "/images/:id/:filename",
+    :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
