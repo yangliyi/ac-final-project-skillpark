bbd971391e67d2f6eba958591bfd973f6b0700eb
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-11-02 02:31:56 +0800

    Change display size to optimize loading speed

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,7 +5,7 @@ class Picture < ActiveRecord::Base
   has_attached_file :image,
     :path => ":rails_root/public/images/:id/:style/:filename",
     :url  => "/images/:id/:style/:filename",
-    :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }
+    :styles => { :large => "600x600>", :medium => "400x400>", :thumb => "100x100>" }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
