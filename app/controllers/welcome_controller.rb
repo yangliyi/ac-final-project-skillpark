ea1c4c91ebe95b7fb1e55a7aaa51394f3f878623
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-11-02 07:35:32 +0800

    Add welcome page

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  layout "landingpage"
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Rails.application.routes.draw do
 
 
   # You can have the root of your site routed with "root"
+  # root 'welcome#index'
   root 'skills#index'
 
   # Example of regular route:
