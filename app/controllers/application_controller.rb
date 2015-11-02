8b9b0405cd13a965417c17b093191b3a7a54f2e2
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-11-02 17:20:01 +0800

    Change after_sign_in_path to skills_path and change after_sign_out_path to root_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_in_path_for(resource)
+    skills_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
 end
