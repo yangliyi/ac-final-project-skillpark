79fcd625323419efdaa6ac8e011a4ea6e690bb81
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-11-03 13:56:35 +0800

    Add comment create api

--- app/controllers/api_v1/comments_controller.rb
+++ app/controllers/api_v1/comments_controller.rb
@@ -1,5 +1,28 @@
 class ApiV1::CommentsController < ApiController
+
+  before_action :authenticate_user_from_token!, :except => [:index]
+
   def index
     @comments = Comment.all
   end
+
+  def create
+    @profile = Profile.find_by_username(params[:data][0][:commented_user] )
+
+    @comment = @profile.comments.create(comment_params)
+    @comment.profile = @profile
+    @comment.user = User.find_by_authentication_token( params[:auth_token] )
+
+    if @comment.save
+      render :json => { :message => "OK", :id => @comment.id }
+    else
+      render :json => { :message => "Validate failed" }, :status => 400
+    end
+
+  end
+
+  def comment_params
+    params.permit(:content)
+  end
+
 end
